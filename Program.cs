3c91051f293728dc49523b17bf455cdc622360c4
zenin5479 <zenin5479@gmail.com>
2025-05-25 11:36:20 +0300

    Update 0.6 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿// Обработка трех массивов
+﻿using System;
+
+// Обработка трех массивов
 // Дано 3 одномерных массива A, B и C разного размера
 // Для каждого из них найти требуемое значение, и затем из трёх полученных выбрать экстремум (наибольшее/наименьшее)
 // Если значения экстремумов совпадают, вывести соответствующее сообщение (с указанием имён массивов с совпадающими значениями)
@@ -6,10 +8,6 @@
 // Имейть в виду, что искомых элементов в массиве(ах) может не быть
 // Быть внимательным при определении типа данных элементов массивов
 // Какой из массивов имеет наименьшее количество отрицательных элементов?
-
-using System;
-
-
 
 namespace Homework_4._4
 {
@@ -20,11 +18,11 @@ namespace Homework_4._4
          // Переводит (,) в (.)
          //System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
 
-         int n = VariousMethods.SizeRow();
-         int m = VariousMethods.SizeColumn();
-         double[,] arrayDouble = VariousMethods.VvodArray(n, m);
+         int n = ClassFor2DArray.SizeRow();
+         int m = ClassFor2DArray.SizeColumn();
+         double[,] arrayDouble = ClassFor2DArray.VvodArray(n, m);
          Console.WriteLine();
-         double[,] arraySearch = VariousMethods.InputArray(arrayDouble, n, m);
+         double[,] arraySearch = ClassFor2DArray.InputArray(arrayDouble, n, m);
          Console.WriteLine();
       }
    }
