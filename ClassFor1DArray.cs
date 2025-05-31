1d9bcdc2c26c8365a47c2f0b7b2cbf15b7d3ce5d
zenin5479 <zenin5479@gmail.com>
2025-05-31 19:32:17 +0300

    Update 11.1 Обновлен метод

--- ClassFor1DArray.cs
+++ ClassFor1DArray.cs
@@ -26,8 +26,8 @@ namespace Homework_4._4
       public static double[] VvodArray(string path)
       {
          string stroka = null;
-         FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read);
-         StreamReader streamReader = new StreamReader(stream);
+         FileStream filestream = File.Open(path, FileMode.Open, FileAccess.Read);
+         StreamReader streamReader = new StreamReader(filestream);
          while (streamReader.Peek() >= 0)
          {
             stroka = streamReader.ReadLine();
@@ -63,9 +63,6 @@ namespace Homework_4._4
 
             Console.WriteLine("Количество столбцов {0}", countСolumn);
          }
-
-
-
 
          // Разделение строки на подстроки по пробелу и конвертация подстрок в double
          Console.WriteLine("Одномерный числовой массив");
